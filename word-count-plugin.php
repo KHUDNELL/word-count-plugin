2cbd70904cd87856065f18b336af317c3810eb01
Ken Hudnell <khudnell252@gmail.com>
2022-09-07 16:44:09 -0400

    added functionality to append plugin content before or after post

--- word-count-plugin.php
+++ word-count-plugin.php
@@ -10,13 +10,30 @@ class WordCountAndTimePlugin {
     function __construct() {
         add_action('admin_menu', array($this, 'adminPage'));
         add_action('admin_init', array($this, 'settings'));
+        add_filter('the_content', array($this, 'ifWrap')); 
+    }
+    function ifWrap($content) {
+        if (is_main_query() AND is_single() AND (get_option('wcp_wordcount', '1') OR get_option('wcp_charcount', '1') OR get_option('wcp_readtime', '1'))) {
+            return $this->createHTML($content);
+        }
+        return $content;
+    }
+
+    function createHTML($content) {
+        $html = '<h3>' . get_option('wcp_headline', 'Post Statistics') . '</h3><p>';
+
+        if (get_option('wcp_location', '0') == '0') {
+            return $html . $content;
+        }
+        return $content . $html;
+
     }
 
     function settings() {
         add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
 
         add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
-        register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+        register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => array($this, 'sanitizeLocation'), 'default' => '0'));
 
         add_settings_field('wcp_headline', 'Headline Text', array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');
         register_setting('wordcountplugin', 'wcp_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics'));
@@ -32,6 +49,14 @@ class WordCountAndTimePlugin {
 
 
 
+    }
+
+    function sanitizeLocation($input) {
+        if ($input != '0' AND $input != '1') {
+            add_settings_error('wcp_location', 'wcp_location_error', 'Display location must be either beginning or end');
+            return get_option('wcp_location');
+        }
+        return $input;
     }
 
     function checkboxHTML($args) {  ?>
